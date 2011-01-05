70017847e81c0785d43faac255291c8db1fcb8be
Dan Tracy <djt5019@psu.edu>
2011-01-05 04:22:27 -0500

    Fixed the move script a bit

--- moveFile.py
+++ moveFile.py
@@ -3,48 +3,81 @@
 #Date: Jan 5, 2011
 
 
-from os import path, walk, rename
-from glob import glob
+import os, glob, shutil
 
 class Move():
 
+        __exts__ = {}
+        
 	@staticmethod
-	def addExtensions( exts=[], extmapping={}, resetExts=False ):
+	def addExtensions( exts=None, resetExts=False ):
+
 		if resetExts:
-			Move.__exts__ = None
-			Move.__extmap__ = None
+			Move.__exts__ = {}
 
-		Move.__exts__ = exts
-		Move.__extmap__ = extmapping
+                if exts is None:
+                        raise Exception("There were no extensions passed")
+                
+		if type(exts) is list:
+                        for ext in exts:
+                                Move.__exts__[str(ext)] = str(ext)
+                elif type(exts) is str:
+                        Move.__exts__[exts] = exts
+                elif type(exts) is dict:
+                        for ext,key in exts.items():
+                                Move.__exts__[ext] = key
+                else:
+                        raise Exception("Illegal extension argument type")
+
+
 
 	@staticmethod
-	def renameExt( srcPath, recurseIntoFolders=False ):
-		if Move.__extmap__ == {}:
+	def renameExt( srcPath='.', parentPath=None, recurseIntoFolders=False ):
+		if Move.__exts__ == {}:
 			return
 
-		if recurseIntoFolders:
-			for root, dirs, files in os.walk(srcPath):
-				for f in files:
-					name,ext = os.path.splitext(f)
+                os.chdir(srcPath)
+                                
+		for f in glob.glob('*'):
+                        name,ext = os.path.splitext(f)
 
-					if ext in Move.__extmap__:
-						try:
-							os.rename(f, name+Move.__extmap__[ext])
-						except Exception, msg:
-							print msg
-		else:
-			for f in glob.glob('*'):
-				name,ext = os.path.splitext(f)
-				if os.path.isfile(f) and ext in Move.__extmap__:
-					os.rename(f, name+Move.__extmap__[ext])
+                        if os.path.isdir(f) and recurseIntoFolders:
+                                Move.renameExt( os.path.abspath(f), srcPath )
+                        elif os.path.isfile(f) and ext in Move.__exts__:
+                                newname = name + Move.__exts__[ext]
+                                os.rename(f, newname)
+
+                if parentPath is not None:
+                        os.chdir(parentPath)
+
 
 	@staticmethod
-	def move( srcPath, destPath ):
-		for root, dirs, files in os.walk(srcPath):
-			for f in files:
-				name,extension = os.path.splitext(f)
-				if extensions in Move.__exts__ or Move.__exts__ == []:
-					try:
-						shutil.move(f, destPath)
-					except Exception as msg:
-						print msgp
+	def move( destPath=None, srcPath='.', parentPath=None, recurseIntoFolders=False ):
+
+                if Move.__exts__ == {}:
+                        raise Exception("The list of extensions is empty")
+                elif destPath is None:
+                        raise Exception("There is no destination path defined")
+                
+                
+                try:
+                        os.chdir(srcPath)
+                except Exception, msg:
+                        print msg
+                        return
+
+                print os.getcwd()
+                                
+		for f in glob.glob('*'):
+                        name,ext = os.path.splitext(f)
+
+                        if os.path.isdir(f) and recurseIntoFolders:
+                                Move.move( os.path.abspath(f), destPath, srcPath )
+                        elif os.path.isfile(f) and ext in Move.__exts__:
+                                try:
+                                        shutil.move(f, destPath)
+                                except Exception, msg:
+                                        print msg
+
+                if parentPath is not None:
+                        os.chdir(parentPath)
