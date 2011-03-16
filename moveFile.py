bc487186ae829862289d8f71bc2243a190798d28
Dan Tracy <djt5019@psu.edu>
2011-03-16 01:18:43 -0400

    fixed something

--- moveFile.py
+++ moveFile.py
@@ -1,81 +1,48 @@
 #Author: Dan Tracy
 #Name: moveFile.py
 #Date: Jan 5, 2011
+#intended for python 2.7
 
+def renameExts( path = None, extensions = None ):
+	""" Renames the files in the path given with the dictionary of extensions"""
+	if path is None:
+		raise Exception("Empty path passed to function")
+    if extensions is None or type(extensions) is not dict:
+        raise Exception("Invalid type for extensions dictionary")
 
-import os, glob, shutil
+	#try not to mangle the namespace as much
+	import os
+	
+	for f in os.listdir(path):
+		name,ext = os.path.splitext(f)
+		name = os.path.join(path,name)
+		#strip out the preceding dot from the filename
+		ext = ext.replace('.','')
+		
+		if ext in extensions:
+			f = os.path.join(path, f)
+			name = name + '.' + extensions[ext]
 
-class Move():
-
-        __exts__ = {}
-        
-	@staticmethod
-	def addExtensions( exts=None, resetExts=False ):
-
-		if resetExts:
-			Move.__exts__ = {}
-
-                if exts is None:
-                        raise Exception("There were no extensions passed")
-                
-		if type(exts) is list:
-                        for ext in exts:
-                                Move.__exts__[str(ext)] = str(ext)
-                elif type(exts) is str:
-                        Move.__exts__[exts] = exts
-                elif type(exts) is dict:
-                        for ext,key in exts.items():
-                                Move.__exts__[ext] = key
-                else:
-                        raise Exception("Illegal extension argument type")
-
-
-
-	@staticmethod
-	def renameExt( srcPath='.', parentPath=None, recurseIntoFolders=False ):
-		if Move.__exts__ == {}:
-			return
-
-                os.chdir(srcPath)
-                                
-		for f in glob.glob('*'):
-                        name,ext = os.path.splitext(f)
-
-                        if os.path.isdir(f) and recurseIntoFolders:
-                                Move.renameExt( os.path.abspath(f), srcPath )
-                        elif os.path.isfile(f) and ext in Move.__exts__:
-                                newname = name + Move.__exts__[ext]
-                                os.rename(f, newname)
-
-                if parentPath is not None:
-                        os.chdir(parentPath)
-
-
-	@staticmethod
-	def move( destPath=None, srcPath='.', parentPath=None, recurseIntoFolders=False ):
-
-                if Move.__exts__ == {}:
-                        raise Exception("The list of extensions is empty")
-                elif destPath is None:
-                        raise Exception("There is no destination path defined")
-                
-                
-                try:
-                        os.chdir(srcPath)
-                except Exception, msg:
-                        print msg
-                        return
-                                
-		for f in glob.glob('*'):
-                        name,ext = os.path.splitext(f)
-
-                        if os.path.isdir(f) and recurseIntoFolders:
-                                Move.move( os.path.abspath(f), destPath, srcPath )
-                        elif os.path.isfile(f) and ext in Move.__exts__:
-                                try:
-                                        shutil.move(f, destPath)
-                                except Exception, msg:
-                                        print msg
-
-                if parentPath is not None:
-                        os.chdir(parentPath)
+			try:
+				os.rename(f, name)
+			except Exception as ex:
+				print ex
+	
+	
+def zipdir(path=None, archiveName = "test.zip"):
+	if path is None:
+		raise Exception("Empty path passed to function")
+		
+	import zipfile, os
+	
+	parent = os.getcwd()
+	os.chdir( path )
+	#So we dont accidentally include the zipfile we create in our file list
+	files = os.listdir(path)
+	
+	with zipfile.ZipFile(archiveName, mode='w', compression=zipfile.ZIP_DEFLATED) as z:
+		for f in files:
+			z.write(f)
+			
+	os.chdir(parent)
+	
