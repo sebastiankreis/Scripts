35c27cc8dd6ea519e362b6987bf37b1a80acb554
Dan Tracy <djt5019@psu.edu>
2011-05-19 00:26:09 -0400

    Changes

--- python/texSynth.py
+++ python/texSynth.py
@@ -12,26 +12,22 @@ except ImportError:
 
 
 class Synth:
-
     ## Synth Constructor
-    def __init__(self, textureFilename=None, texton=(5,5), size=(256,256)):
-        if textureFilename == None:
-            raise Exception("Missing texture file")
-        
+    def __init__(self, textureFilename, texton, size, outFile):
         self.texture = self.loadTexture(textureFilename)
         self.image = cv.CreateImage(size,cv.IPL_DEPTH_8U, 3)
         cv.SetZero(self.image)
         self.textonSize = texton
         self.size = size
-		
-		self.imageTextons = []
-		self.textureTextons=[]
-		for u in xrange(0, self.image.height, self.textonSize[0]):
-            for v in xrange(0, self.image.width, self.textonSize[1]):
-				self.imageTextons.append( self.getTexton(u,v) )
-				self.textureTextons.append( self.getTexton(u,v) )
-        
+        self.outputFile = outFile
+        self.imageTextons = []
+        self.textureTextons=[]
 
+        for u in xrange(0, self.image.height, self.textonSize[0]):
+            for v in xrange(0, self.image.width, self.textonSize[1]):
+                self.imageTextons.append( self.getTexton(u,v) )
+                self.textureTextons.append( self.getTexton(u,v) )
+                
     ## Retrieve a textonSize by textonSize square from the image
     def getTexton(self, image, x=0, y=0):
         x  = max( x - self.textonSize[0], 0)
@@ -39,15 +35,13 @@ class Synth:
         dx = min( x + self.textonSize[0], image.width)
         dy = min( y + self.textonSize[1], image.height)
         return image[x:dx, y:dy]
-    
-    
+
     def copyTexture(self, image, tex):
         ''' Copy the texture tex subrectangle to the image subrectangle'''
         for u in xrange(tex.height):
             for v in xrange(tex.width):
                 image[u,v] = tex[u,v]
-
-
+                
     ## Hardcore quilting action
     def quilt(self):
         #random texture texton for the upper left corner
@@ -56,14 +50,14 @@ class Synth:
         textureTexton = self.getTexton( self.texture, i, j)
         imageTexton = self.imageTexton[0]
         self.copyTexture(imageTexton, textureTexton)
-        
-        for tex in self.imageTextons[1:]:                    
+
+        for tex in self.imageTextons[1:]:
             previousTexton = textureTexton
             imageTexton = tex
             textureTexton = self.searchTexture(previousTexton)
             #textureTexton = self.blend(textureTexton,previousTexton)
             self.copyTexture(imageTexton, textureTexton)
-
+            
     ## This utilizes my distance metric, I use a relative L2 norm to compare two overlapping regions
     def searchTexture(self, tex):
         overlap = self.textonSize[0] / 3
@@ -72,12 +66,11 @@ class Synth:
         minSSD = 1000
         index = (0,0)
         currentTile = tex
-        
+
         for candidate in self.textureTextons:
             candCut= self.getCut(candidate,0,0,overlap,candidate.height)
-
             result = cv.Norm(texCut, candCut, cv.CV_RELATIVE_L2)
-    
+
             if minSSD > result:
                 index = (u,v)
                 minSSD = result
@@ -87,15 +80,13 @@ class Synth:
 
     def blend(self, newTexton, prevTexton):
         pass
-
     
     ## Due to a bug in OpenCV (Ticket 528) I had to implement this kludge.
     def getCut(self, image, xBegin, yBegin, xEnd, yEnd):
         dx = abs(xEnd-xBegin)
-        dy = abs(yEnd-yBegin) 
-        
+        dy = abs(yEnd-yBegin)
+
         s = cv.CreateMat(dy, dx, image.type)
-        
         for u in xrange(s.height):
             for v in xrange(s.width):
                 s[u,v] = image[u+yBegin,v+xBegin]
@@ -108,42 +99,46 @@ class Synth:
         try:
             tex = cv.LoadImage(filename, cv.CV_LOAD_IMAGE_COLOR)
         except Exception:
-            print "Texture could not be loaded"
+            print "Texture {0} could not be loaded".format(filename)
+            exit(1)
         finally:
             return tex
 
     ## I choose to save the images as PNGs to try to avoid some
     ## of the compression artifacts from JPEG
-    def saveImage(self, filename="out.png"):
-        cv.SaveImage(filename, self.image)
-
-    ## End of the Synth Class
-
-
-
-def checkInput(inp, varName):
-    string = "Error: {0} needs to be a valid integer greater than zero.".format(varName)
-    try:
-        result = int(inp)
-        if inp < 0:
-            sys.exit( string )
-
-    except ValueError:
-        sys.exit( string )
-
-    return result
+    def saveImage(self):
+        cv.SaveImage(self.outputFile, self.image)
 
 
 if __name__ == '__main__':
-    
-    if len(sys.argv) != 4:
-        print "Usage: [texture filename] [texton size] [output image size] "
-        sys.exit(1)
-        
-    texFile = sys.argv[1]
-    texSize = checkInput(sys.argv[2], 'Texton Size')
-    size = checkInput(sys.argv[3], 'Size')
+    import argparse
+    import sys
 
-    x = Synth( texFile, (texSize,texSize), (size,size) )
-    x.quilt()
-    x.saveImage()
+    test = "-i test"
+
+    parser = argparse.ArgumentParser(description="Texture Synthesis Program")
+    parser.add_argument('-i','--input-file', required=True, type=str,
+                        metavar='f', help='Input image filename')
+    
+    parser.add_argument('-t','--texton-size', type=int, default=5,
+                        metavar='n', help="default is 5 pixels")
+    
+    parser.add_argument('-o','--output-file', type=str, default='output.png',
+                        metavar='f', help="Output image filename")
+    
+    parser.add_argument('-s','--output-size', type=int, default=256,
+                        metavar='n', help="default is 256 pixels")
+
+    namespace = parser.parse_args(test.split())
+    texFile = namespace.input_file
+    texSize = namespace.texton_size
+    size    = namespace.output_size
+    outFile = namespace.output_file
+
+    
+    print namespace
+    
+    
+    generatedImage = Synth( texFile, (texSize,texSize), (size,size), outFile )
+    generatedImage.quilt()
+    generatedImage.saveImage()
