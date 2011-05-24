d77f9c5dc7ab380cc02d665f2e32cb22b9bceb16
Dan Tracy <djt5019@psu.edu>
2011-05-23 22:37:43 -0400

    Added a sqlite database to the episode program... because I can

--- python/eplist.py
+++ python/eplist.py
@@ -3,6 +3,12 @@
 import urllib2 as urllib
 import re
 import string
+from contextlib import closing
+
+try:
+        import sqlite3 as sql
+except ImportError:
+        sql = None
 
 #output display format
 #Season - Episode Number - Episode Title
@@ -13,58 +19,79 @@ class Episode(object):
 		self.title = title
 		self.season = season
 		self.episode = epNumber
+		
 	def __repr__(self):
 		global display
 		return display.format(self.season,self.episode,self.title)
 			
 class EpParser(object):
-	def __init__(self, showTitle):
-		self.title = self.__prepareTitle(showTitle.lower())
-		self.url = "http://epguides.com/" + self.title
+	def __init__(self, showTitle, cache=None):
+                ''' Proper title is used for the database/url while the display
+                title is used for error messages/display purposes'''
+                
+		self.properTitle = self._prepareTitle(showTitle.lower())
+		self.displayTitle = showTitle
+		
+		self.url = "http://epguides.com/" + self.properTitle
 		self.episodeList = []
-		self.parseData()
+		self.cache = cache
 
-	def __prepareTitle(self, title):
-		'''Remove any punctuation and whitespace from the title'''
-		exclude = set(string.punctuation)
-		title = ''.join(ch for ch in title if ch not in exclude)
-		title = title.split()
-		if title[0] == 'the': title.remove('the')
-		return ''.join(title)
-		
-	def __getData(self):
-		try:
-			request = urllib.urlopen( self.url )
-			return request.readlines()
-		except urllib.HTTPError as e:
-			print '{0}'.format(e)
-			if e.getcode == 404:
-				print 'Show {0} was not found, please check your spelling.'.format(self.title)
-			exit()
-		except urllib.URLError as e:                        
-			print '{0}'.format(e)
-		
+	def _prepareTitle(self, title):
+                '''Remove any punctuation and whitespace from the title'''
+                
+                exclude = set(string.punctuation)
+                title = ''.join(ch for ch in title if ch not in exclude)
+                title = title.split()
+                if title[0] == 'the': title.remove('the')
+                return ''.join(title)
 
 	def parseData(self):
-		'''Requests the title's webpage from the URL, parses information,
-		   and populates the episodeList'''
-		data = self.__getData()
-		
+                if self.cache is not None:
+                        self.episodeList = self._parseCacheData()
+
+                # The show was found in the database
+                if self.episodeList != []:
+                        self.cache.close()
+                        return 
+
+                self.episodeList = self._parseHTMLData()
+
+                if self.cache is not None:
+                        self.cache.addShow( self.properTitle, self.episodeList )
+                        self.cache.close()
+                        
+        def _parseCacheData(self):
+                showId = self.cache.getShowId(self.properTitle)
+                
+                if showId == -1: return []
+
+                return self.cache.getEpisodes(showId)
+                
+                 
+
+        def _getHTMLData(self):
+                with closing(urllib.urlopen( self.url )) as request:
+                        if request.getcode() == 200:
+                                return request.readlines()
+                        
+	def _parseHTMLData(self):
+                data = self._getHTMLData()
+		episodes = []
 		pattern = r"""
-			^			                # Start of the string
-			(?:[\s]*?[\d]*\.?)		        # Number on list
-			[\s]{2,}			        # Ignore whitespace
-			(?P<season>[\d]*)		        # Season number
-			-				        # Separator
-			[\s]*				        # Optional whitespace
-			(?P<episode>[\d]*)		        # Episode number
-			[\s]{2,}			        # Ignore whitespace
-			(?P<product>.+|)		        # Product number, or nothing
-			[\s]{2,}				# Ignore whitespace
-			(?P<airdate>[\w\s/]*?)	                # Air-date
-			[\s]{2,}				# Ignore whitespace
-			(?P<name>.*)			        # Episode name
-			$					# End of line
+			^		        # Start of the string
+			(?:[\s]*?[\d]*\.?)	# Number on list
+			[\s]{2,}		# Ignore whitespace
+			(?P<season>[\d]*)	# Season number
+			-			# Separator
+			[\s]*			# Optional whitespace
+			(?P<episode>[\d]*)	# Episode number
+			[\s]{2,}		# Ignore whitespace
+			(?P<product>.+|)	# Product number, or nothing
+			[\s]{2,}		# Ignore whitespace
+			(?P<airdate>[\w\s/]*?)	# Air-date
+			[\s]{2,}		# Ignore whitespace
+			(?P<name>.*)		# Episode name
+			$			# End of line
 			"""
 		regex = re.compile(pattern, re.X|re.I )
 
@@ -75,34 +102,116 @@ class EpParser(object):
 				e = info.group('episode')
 				s = int(info.group('season'))
 				n = re.sub('<.*?>', '', n).strip()
-				self.episodeList.append( Episode(n,e,s) )
-				
+				episodes.append( Episode(n,e,s) )
+		return episodes
 
+class Cache(object):
+        def __init__(self, recreate=False, db="episodes.db"):
+                self.connection = sql.connect(db)
+                self.cursor = self.connection.cursor()
+                if recreate == True:
+                        print "Making a new cache"
+                        self.cursor.executescript( '''
+                                DROP TABLE episodes;
+                                DROP TABLE shows;
+
+                                CREATE TABLE shows (	
+                                        sid INTEGER PRIMARY KEY, 
+                                        title TEXT
+                                        );
+
+                                CREATE TABLE episodes ( 
+                                        eid INTEGER PRIMARY KEY, 
+                                        sid INTEGER, eptitle TEXT, 
+                                        season INTEGER, 
+                                        showNumber INTEGER
+                        );''')
+
+        def close(self):
+                self.cursor.close()
+                self.connection.commit()
+                self.connection.close()
+
+        def getShowId(self, showTitle):
+                title = (showTitle,)
+                self.cursor.execute("SELECT sid FROM shows WHERE\
+                                        title=?", title)
+                result = self.cursor.fetchone()
+                if result is not None:
+                        return result[0]
+                else:
+                        return -1
+
+        def getEpisodes(self, showId):
+                sid = (showId,)
+                self.cursor.execute(
+                        "SELECT eptitle, shownumber, season FROM episodes\
+                         WHERE sid=?",sid)
+                
+                result = self.cursor.fetchall()
+                eps = []
+                
+                if result is not None:
+                        for episode in result:
+                                eps.append( Episode(*episode) )
+
+                return eps
+
+                
+        def addShow(self, showTitle, episodes):
+                title = (showTitle,)
+                self.cursor.execute("INSERT INTO shows values (NULL, ?)", title)
+                showId = self.getShowId(showTitle)
+                for e in episodes:
+                        show = (showId,e.title, e.season, e.episode,)
+                        self.cursor.execute(
+                                "INSERT INTO episodes values (NULL, ?, ?, ?, ?)", show)
+        
 def main():
 	import argparse
 	import sys
-	sys.argv = '-s 1 "The Wire"'.split()
 
-	parser = argparse.ArgumentParser()
+	parser = argparse.ArgumentParser(description="Episode Parser")
+
 	parser.add_argument('title',
 			    help="The title of the show")
-	parser.add_argument('-s','--season', default=-1, type=int,
+	
+	parser.add_argument('-s','--season', default=-1, type=int, metavar='n',
 			    help="The specific season to search for")
+	
 	parser.add_argument('-d','--display-header', action="store_true",
 			    help="Display the header at the top of the output")
-	namespace = parser.parse_args()
-
-	title = namespace.title
-	season= namespace.season
 	
-	ep = EpParser(title)
+	parser.add_argument('-v', '--verbose', action="store_true",
+                            help="Be verbose")
+	
+	parser.add_argument('-n', '--nocache', action="store_true",
+                            help="Disallow the program to load the episode database")
+
+        parser.add_argument('-r', '--recreatecache', action="store_true",
+                            help="Will recreate the cache from scratch, be sure you want to")
+        
+	namespace = parser.parse_args()
+	
+	title   = namespace.title
+	season  = namespace.season
+	noCache = namespace.nocache
+	newCache= namespace.recreatecache
+
+        if sql is None or noCache == True:
+                cache = None
+        else:
+                cache = Cache(recreate=newCache)
+                
+	ep = EpParser(title, cache)
+	ep.parseData()
 	results = ep.episodeList
 
-	header  = "Show: {0}\n------" + ('-'*len(title))
 	if season != -1:
 		results = filter(lambda x: x.season == season, results)
 
-	if namespace.display_header:
+	if namespace.display_header or namespace.verbose:
+		header  = "Show: {0}\n------" + ('-'*len(title))
 		print header.format(title)
 		
 	for eps in results:
@@ -110,5 +219,4 @@ def main():
 
 
 if __name__ == '__main__':
-	main()
-	
+	main()	
