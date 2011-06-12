af1c57bd46bcc778d3af76f6bd9f346611af1dd5
Dan Tracy <djt5019@psu.edu>
2011-06-12 03:11:14 -0400

    Messed around with the move file junk

--- python/moveFile.py
+++ python/moveFile.py
@@ -1,74 +1,99 @@
-#Author: Dan Tracy
 #!/usr/bin/env python
+# Author: Dan Tracy
 # Name: moveFile.py
 # Date: Jan 5, 2011
 # intended for python 2.7
 
 import os
 import zipfile
+from itertools import iterzip
+from collections import OrderedDict
 
-def renameExts( path = None, extensions = {'zip':'cbz','rar':'cbr'}, recurseIntoFolders=False ):
+comicExt = {'zip':'cbz','rar':'cbr'}
+def renameExts( path = None, extensions =comicExt, recurseIntoFolders=False ):
         """ Renames the files in the path given with the dictionary of extensions"""
-        if path is None:
-                raise Exception("Empty path passed to function")
-        if extensions is None or type(extensions) is not dict:
-                raise Exception("Invalid type for extensions dictionary")
- 
+        if path is None or extensions is None:
+                return None
+
         for f in os.listdir(path):
                 name,ext = os.path.splitext(f)
                 name = os.path.join(path,name)
 
-                if os.path.isdir(name+ext) and recurseIntoFolders:
-                        renameExts( name+ext, extensions, True)
+                if os.path.isdir(name) and recurseIntoFolders:
+                        renameExts( name, extensions, True)
                 else:
                         #strip out the preceding dot from the filename
                         ext = ext.replace('.','')
                         if ext in extensions:
                                 f = os.path.join(path, f)
                                 name = name + '.' + extensions[ext]
-                                try:
-                                        os.rename(f, os.path.join(path,name))
-                                except Exception as ex:
-                                        print ex
+                                os.rename(f, os.path.join(path,name))
+              
 
-def renameFiles( path=None, names = None ):
-        if path is None or names is None:
-                raise Exception
+def renameFiles( path=None, names = None, testRun=False ):
+	if path is None or names is None:
+			raise Exception
+ 
+	# We will sort dictionary with respect to key value by
+	# removing all punctuation then adding each entry into
+	# a ordered dictionary to preserve sorted order.  We do
+	# this to insure the file list is in somewhat of a natural
+	# ordering, otherwise we will have misnamed files
+		
+	files = os.listdir(path)
+	clean = map(__removePunc, files)
+	cleanFiles = dict(izip(clean,files))
+	cleanFiles = orderedDictSort(cleanFiles)
+		
+	for f,n in izip(cleanFiles.iterkeys(),names):
+		fileName = cleanFiles[f]
+		_, ext   = os.path.splitext(f)
+		newName  = n.strip() + ext
 
-        files = os.listdir(path)
-        for f,n in zip(files,names):
-                _,ext = os.path.splitext(f)
-                n = n.strip() + ext
-                n = os.path.join(path,n)
-                f = os.path.join(path,f)
-                print f
-                print n
-                print ""
-                os.rename(f,n)
-       
-                
+		fileName = os.path.join(path, fileName)
+		newName  = os.path.join(path, newName)
+
+		print ("OLD: {0}".format(fileName))
+		print ("NEW: {0}".format(newName))
+		print ""
+				
+		if not testRun: os.rename(fileName,newName)
+	   
+				
 
 def zipDirs(topPath = None, ext = 'zip'):
-        topPath = os.path.realpath(topPath)
-        parent = os.getcwd()
-        os.chdir(topPath)
+	topPath = os.path.realpath(topPath)
+	parent = os.getcwd()
+	os.chdir(topPath)
 
-        def __zipdir(path=None, parent=None, archiveName = "test.zip"):
-                files = os.listdir(path)
-                with zipfile.ZipFile(archiveName, mode='w', compression=zipfile.ZIP_DEFLATED) as z:
-                        os.chdir(path)
-                        for f in files:
-                                z.write(f)
-                        os.chdir(parent)
-        
-        for dirs in os.listdir(topPath):
-                name = dirs
-                dirs = os.path.realpath(dirs)
-                if os.path.isdir(dirs):
-                        print "Zipping {0}".format(dirs)
-                        __zipdir(dirs, topPath, dirs+'.'+ext)
+	def __zipdir(path=None, parent=None, archiveName = "test.zip"):
+		files = os.listdir(path)
+		with zipfile.ZipFile(archiveName, mode='w', compression=zipfile.ZIP_DEFLATED) as z:
+			os.chdir(path)
+			for f in files:
+				z.write(f)
+			os.chdir(parent)
+		
+		for dirs in os.listdir(topPath):
+			name = dirs
+			dirs = os.path.realpath(dirs)
+			if os.path.isdir(dirs):
+				print "Zipping {0}".format(dirs)
+				__zipdir(dirs, topPath, dirs+'.'+ext)
 
-        os.chdir(parent)
-                
+		os.chdir(parent)
+				
 
-                 
+def __removePunc(title):
+	import string
+	name,ext = os.path.splitext(title)
+	'''Remove any punctuation and whitespace from the title'''
+	exclude = set(string.punctuation)
+	name = ''.join(ch for ch in name if ch not in exclude)
+	return name+ext
+
+def orderedDictSort(dictionary):
+	keys = dictionary.keys()
+	keys.sort()
+	d = zip(keys, map(dictionary.get,keys))
+	return OrderedDict(d)
