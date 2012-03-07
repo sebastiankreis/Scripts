101be3a1b474b1ec68eb30d7204528a3215c42c4
Dan Tracy <djt5019@psu.edu>
2012-03-07 14:10:53 -0500

    Added a django test site

---
+++ python/django_test/test_site/polls/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from datetime import datetime, timedelta
+
+
+# Create your models here.
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __unicode__(self):
+        return self.question
+
+    def published_recently(self):
+        return self.pub_date >= (datetime.now() - timedelta(days=1))
+
+    published_recently.admin_order_field = 'pub_date'
+    published_recently.boolean = True
+    published_recently.short_description = "Published Recently?"
+
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice = models.CharField(max_length=200)
+    votes = models.IntegerField()
+
+    def __unicode__(self):
+        return self.choice
